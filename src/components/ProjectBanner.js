47a9d4c94105bbd354d5145fb342e5fc89a59a72
sillysloth888 <115318787+sillysloth888@users.noreply.github.com>
2022-12-16 18:14:41 -0500

    Delete src directory

--- src/components/ProjectBanner.js
+++
@@ -1,9 +0,0 @@
-import styles from "./ProjectBanner.module.css"
-
-const ProjectBanner = ({ imgSrc }) => (
-  <div className={styles["img-container"]}>
-    <img src={imgSrc}/>
-  </div>
-)
-
-export default ProjectBanner
